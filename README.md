348de913032340e3e8671e1a9645adae5d660643
haolun <haolun@example.com>
2018-06-03 01:29:40 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# spring-cloud-eureka-server
+spring-cloud-eureka-server
+
+see: https://blog.asarkar.org/technical/netflix-eureka/
+see: https://github.com/Netflix/eureka/wiki
---
+++ src/main/java/cn/home1/cloud/netflix/eureka/server/EurekaServer.java
@@ -0,0 +1,37 @@
+package cn.home1.cloud.netflix.eureka.server;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.event.EventListener;
+
+/**
+ * Created by zhanghaolun on 16/9/26.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+@Slf4j
+public class EurekaServer {
+
+  @Autowired
+  private EurekaClientConfigBean eurekaClientConfig;
+
+  @Autowired
+  private EurekaInstanceConfigBean eurekaInstanceConfig;
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void printImportantInfo() {
+    log.info("eureka.client.service-url.defaultZone: {}", this.eurekaClientConfig.getServiceUrl().get("defaultZone"));
+    log.info("eureka.instance.hostname: {}", this.eurekaInstanceConfig.getHostname());
+  }
+
+  public static void main(final String... args) {
+    new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
+  }
+}
