9727947e0810529488d6e75a61e87fe21ff11720
haolun zhang <opensource@infra.top>
2018-09-07 15:38:15 +0800

    update ci script, dependencies, docker-compose

--- spring-cloud-service-filter/src/test/java/cn/home1/cloud/netflix/eureka/client/EurekaInstanceTestApplication.java
+++ spring-cloud-service-filter/src/test/java/cn/home1/cloud/netflix/eureka/client/EurekaInstanceTestApplication.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //@EnableEurekaClient
