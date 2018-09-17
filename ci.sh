571b5f449f939e54e209947cedd5ab82eea2db22
haolun zhang <opensource@infra.top>
2018-09-18 01:12:27 +0800

    upgrade parent

--- ci.sh
+++ ci.sh
@@ -4,13 +4,13 @@
 #echo -e "\n>>>>>>>>>> ---------- options in travis-ci's .travis.yml, this is for local test or debug ---------- >>>>>>>>>>"
 #export CI_INFRA_OPT_GIT_AUTH_TOKEN="${CI_INFRA_OPT_OPENSOURCE_GIT_AUTH_TOKEN}"
 #export CI_INFRA_OPT_OPENSOURCE_GIT_PREFIX="https://github.com"
-#export CI_OPT_CI_SCRIPT="https://github.com/ci-and-cd/maven-build/raw/v0.2.2/src/main/ci-script/lib_ci.sh"
+#export CI_OPT_CI_SCRIPT="https://github.com/ci-and-cd/maven-build/raw/v0.2.3/src/main/ci-script/lib_ci.sh"
 #export CI_OPT_INFRASTRUCTURE="opensource"
 #echo -e "<<<<<<<<<< ---------- options in travis-ci's .travis.yml, this is for local test or debug ---------- <<<<<<<<<<\n"
 
 
 echo -e "\n>>>>>>>>>> ---------- custom, override options ---------- >>>>>>>>>>"
-if [ -z "${CI_OPT_CI_SCRIPT}" ]; then CI_OPT_CI_SCRIPT="https://github.com/ci-and-cd/maven-build/raw/v0.2.2/src/main/ci-script/lib_ci.sh"; fi
+if [ -z "${CI_OPT_CI_SCRIPT}" ]; then CI_OPT_CI_SCRIPT="https://github.com/ci-and-cd/maven-build/raw/v0.2.3/src/main/ci-script/lib_ci.sh"; fi
 if [ -z "${CI_OPT_DOCKER_IMAGE_PREFIX}" ]; then CI_OPT_DOCKER_IMAGE_PREFIX="cloudready/"; fi
 if [ -z "${CI_OPT_GITHUB_SITE_REPO_OWNER}" ]; then CI_OPT_GITHUB_SITE_REPO_OWNER="cloud-ready"; fi
 if [ -z "${CI_OPT_GPG_KEYNAME}" ]; then CI_OPT_GPG_KEYNAME="59DBF10E"; fi
